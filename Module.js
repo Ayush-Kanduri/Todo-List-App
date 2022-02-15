7df9087ce61c3df229e8eeb4f9a283abdf65b607
Ayush <ayush.kanduri@gmail.com>
2022-02-15 20:48:57 +0530

    Declarations & Initialisations

--- Module.js
+++ Module.js
@@ -1,6 +1,22 @@
 //Revealing Module Pattern using IIFE Module Design Pattern.
 //----------------------------------------------------------------
 let todoApp = (() => {
+	let taskList = [];
+	const inputTask = document.getElementById("task");
+	const submitTask = document.getElementById("taskSubmit");
+	const upperTabs = document
+		.querySelectorAll(".tabs-box")[0]
+		.querySelectorAll(".tabs");
+	const lowerTabs = document
+		.querySelectorAll(".tabs-box")[1]
+		.querySelectorAll(".tabs");
+	const allTabContent = document.querySelectorAll(".scroll > .content")[0];
+	const incompleteTabContent =
+		document.querySelectorAll(".scroll > .content")[1];
+	const completedTabContent =
+		document.querySelectorAll(".scroll > .content")[2];
+	let taskItemTemplate = allTabContent.querySelector(".task").cloneNode(true);
+	taskItemTemplate.style.display = "flex";
 	//----------------------------------------------------------------
 	//Function: Handles the Click Events in the Todo List App//
 	let handleClick = (event) => {
