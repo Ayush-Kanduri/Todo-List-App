4e7921ace78842335c5dfdb8851da39183866335
Ayush <ayush.kanduri@gmail.com>
2022-02-15 20:47:04 +0530

    Click Handled

--- Module.js
+++ Module.js
@@ -2,6 +2,21 @@
 //----------------------------------------------------------------
 let todoApp = (() => {
 	//----------------------------------------------------------------
+	//Function: Handles the Click Events in the Todo List App//
+	let handleClick = (event) => {
+		//Event.target is the element that was clicked
+		const target = event.target;
+		//If the target is the submit button, then add the task to the list
+		if (target.id === "taskSubmit") {
+			const taskTitle = inputTask?.value;
+			if (taskTitle === "") return;
+			let task = new Task(taskTitle);
+			addTask(task);
+			inputTask.focus();
+			inputTask.value = "";
+		}
+	};
+	//----------------------------------------------------------------
 	//Function: Initializes the Todo List App//
 	let initialiseApp = () => {
 		//Click Event Delegation
