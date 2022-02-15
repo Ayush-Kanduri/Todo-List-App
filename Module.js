a4159405c9a7baaf01e2ad000b2debe8b7717386
Ayush <ayush.kanduri@gmail.com>
2022-02-15 20:45:00 +0530

    Initialise App

--- Module.js
+++ Module.js
@@ -1,4 +1,17 @@
 //Revealing Module Pattern using IIFE Module Design Pattern.
 //----------------------------------------------------------------
-let todoApp = (() => {})();
+let todoApp = (() => {
+	//----------------------------------------------------------------
+	//Function: Initializes the Todo List App//
+	let initialiseApp = () => {
+		//Click Event Delegation
+		document.addEventListener("click", handleClick);
+		//KeyPress Event Delegation
+		document.addEventListener("keyup", handleKeyPress);
+	};
+	//----------------------------------------------------------------
+	return {
+		initialise: initialiseApp,
+	};
+})();
 //----------------------------------------------------------------
